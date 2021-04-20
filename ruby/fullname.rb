48582f68b4f856936cb17e8ccfa0ed8b4b5df0e7
Alyssa Hooper <nesteralyssa@gmail.com>
2021-04-19 23:16:49 -0500

    adds files

---
+++ ruby/fullname.rb
@@ -0,0 +1,11 @@
+# user input
+
+puts "What is your first name?"
+
+first_name = gets.chomp
+
+puts "What is your last name?"
+
+last_name = gets.chomp
+
+puts "Okay, you must be #{first_name} #{last_name}!"
