1bb18b1f93295633cae1d91ede61b6000ee3171d
Alyssa Hooper <nesteralyssa@gmail.com>
2021-04-21 22:26:25 -0500

    updates input and output files

---
+++ ruby/gets.rb
@@ -0,0 +1,5 @@
+puts 'Write a sentence...'
+
+new_sentence = gets.chomp
+
+puts "Your sentence is: " + "#{new_sentence}"
--- ruby/print.rb
+++ ruby/print.rb
@@ -1,4 +1,7 @@
-#puts adds a line after execustion 
+#puts adds a line after execustion
 puts "hello"; puts "I love coffee";
 
+#no line
 print "hello"; print "I love coffee";
+
+#both return nil
