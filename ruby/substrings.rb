b8a80053815cadb6585622b33ac55516d5f3839e
Alyssa Hooper <nesteralyssa@gmail.com>
2021-05-04 23:49:31 -0500

    inital method setup

--- ruby/substrings.rb
+++ ruby/substrings.rb
@@ -1,5 +1,9 @@
+#Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(word, arr)
-
+def substrings(word, dictionary)
+  dictionary.count.include? word
 end
+
+substrings("below", dictionary)
